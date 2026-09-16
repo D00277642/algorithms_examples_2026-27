b9ac2aa63c9033cc3c7825971ef5e34c612cc0dc
D00277642 <D00277642@student.dkit.ie>
2026-09-16 11:40:47 +0100

    all commented out start of exercise 1.2 Conversion Exercises

---
+++ src/main/java/exercises/UserEntersTwoNumbers_Maths.java
@@ -0,0 +1,16 @@
+//conversation exercise class 2
+//Exercise 1.2 Loops
+//
+//package exercises;
+//
+//import java.util.Scanner;
+//
+//public class UserEntersTwoNumbers_Maths {
+//    static void main(){
+//        Scanner input = new Scanner(System.in);
+//        int sum = 0;
+//        int guess = 0;
+//        int counter = 0;
+//
+//    }
+//}
